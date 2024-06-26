0d77929d0b81dbcd9655427326b4a4fb33063df2
elbok <kromsten@pm.me>
2024-06-26 14:17:49 +0200

    rm ibc dublicate tests

--- tests/gateway.test.ts
+++ tests/gateway.test.ts
@@ -1,10 +1,7 @@
-import { expect, describe, it, test, beforeAll } from 'vitest';
+import { expect, describe, it, beforeAll } from 'vitest';
 import { executeGatewayEncrypted, getGatewayEncryptionKey, queryGatewayAuth } from '../src/gateway';
-import { consumerClient, consumerWallet, secretWallet } from '../src/clients';
-import { gatewayChachaHookMemo, gatewayHookMemo, sendIBCToken } from '../src/ibc';
-import { loadContractConfig, loadIbcConfig } from '../src/config';
+import { consumerWallet, secretWallet } from '../src/clients';
 import { getArb36Credential } from '../src/crypto';
-import { CONSUMER_TOKEN } from '../src/env';
 
 describe('Gateway contract interaction', () => {
 
@@ -70,75 +67,4 @@ describe('Gateway contract interaction', () => {
     });
 
 
-    describe('sending a message over ibc', async () => {
-        
-        const consumerQCFirst = await getArb36Credential(consumerWallet, "foo")
-        const consumerQCSecond = await getArb36Credential(consumerWallet, "bar")
-
-        const ibcConfig = loadIbcConfig();
-        const secretGateway = loadContractConfig().gateway!;
-
-
-        test('if can see simple ibc-hook msg', async () => {
-
-            const new_text = "new_text_" + Math.random().toString(36).substring(7);
-
-            // non-authenticated & non-encrypted query
-            // visible to everyone over IBC and the relayer will
-            // be the one who stored the secret
-            const response = await sendIBCToken(
-                consumerClient,
-                secretGateway.address,
-                CONSUMER_TOKEN!,
-                "1",
-                ibcConfig.consumer_channel_id,
-                gatewayHookMemo(
-                    { extension: { msg: { store_secret: { text: new_text } } }},
-                    secretGateway
-                )
-            )
-            console.log("hook res:", response)
-            expect(response).toBeDefined();
-            expect(response.code).toEqual(0);
-
-            
-            const non_updated_text = (await queryGatewayAuth(
-                { get_secret: {} },
-                [consumerQCFirst]
-            )) as string;
-            expect(non_updated_text).not.toEqual(new_text);
-        })
-
-
-        test('sending chacha20_poly1305_seal ibc-hook msg', async () => {
-
-            const new_text = "new_text_" + Math.random().toString(36).substring(7);
-
-            const response = await sendIBCToken(
-                consumerClient,
-                secretGateway.address,
-                CONSUMER_TOKEN!,
-                "1",
-                ibcConfig.consumer_channel_id,
-                await gatewayChachaHookMemo(
-                    consumerWallet,
-                    { extension: { msg: { store_secret: { text: new_text } } } },
-                    secretGateway,
-                    gatewayKey
-                )
-            )
-            console.log("hook res:", response)
-            expect(response).toBeDefined();
-            expect(response.code).toEqual(0);
-
-            
-            const updated_text = (await queryGatewayAuth(
-                { get_secret: {} },
-                [consumerQCSecond]
-            )) as string;
-
-            expect(updated_text).toEqual(new_text);
-
-        });
-    });
 });
